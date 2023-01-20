6642796542e271f2d01590af3d916fb17fe96560
RAHAMSHAIK007 <81603735+RAHAMSHAIK007@users.noreply.github.com>
2023-01-20 07:29:17 +0530

    abc commits

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIATTAU7W3MTSH2GAUR"
+  secret_key = "UkQHd5U5W36FZYY9a2IhpHXhgGRVVjv0zhbAfLs5"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+  key_name = "rahamkeys"
+  avaliabilty_zone = "us-east-1d"
+  tags= {
+    Name = "raham-tf"
+  }
+}
